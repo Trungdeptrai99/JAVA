62402df7d277b2c174422b12ebb08e0a5e1be094
Trungdeptrai99 <ktdarkness99@gmail.com>
2020-07-05 00:45:56 +0700

    update

--- songaytrongthang/src/tinhsongaytrongthang.java
+++ songaytrongthang/src/tinhsongaytrongthang.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 public class tinhsongaytrongthang {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Which month that you want to count days?: ");
+       Scanner scanner = new Scanner(System.in);
+        System.out.print("Which month that you want to count days? ");
         int month = scanner.nextInt();
-
+        String daysInMonth;
         switch (month) {
             case 2:
-                System.out.print("The month '2' has 28 or 29 days!");
+                daysInMonth = "28 or 29";
                 break;
             case 1:
             case 3:
@@ -16,17 +16,20 @@ public class tinhsongaytrongthang {
             case 8:
             case 10:
             case 12:
-                System.out.print("The month '" + month + "' has 31 days!");
+                daysInMonth = "31";
                 break;
             case 4:
             case 6:
             case 9:
             case 11:
-                System.out.print("The month '" + month + "' has 30 days!");
+                daysInMonth = "30";
                 break;
             default:
-                System.out.print("Invalid input!");
-                break;
+                daysInMonth = "";
+        }
+
+        if (daysInMonth != "") System.out.printf("The month '%d' has %s days!", month, daysInMonth);
+        else System.out.print("Invalid input!");
         }
     }
-}
+
