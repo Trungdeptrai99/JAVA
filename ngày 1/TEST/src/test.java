8ddd568f1eb1e67ab4b746d331a3436a5f2f62a0
Trungdeptrai99 <ktdarkness99@gmail.com>
2020-07-05 12:48:08 +0700

    Update

---
+++ ngày 1/TEST/src/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        boolean yourCrush= true;
+        boolean yourGirlFriend= false;
+        while (yourCrush!=yourGirlFriend){
+            System.out.println("hihithangngu");
+            break;
+        }
+    }
+}
