1d255e962b95370494cc15470b275b201f06f01e
Trungdeptrai99 <ktdarkness99@gmail.com>
2020-07-09 22:56:31 +0700

    update

---
+++ ngày 5/baitap/src/TestCircle.java
@@ -0,0 +1,10 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle(10.0,"hihi");
+        System.out.println(circle);
+        Circle a = new Circle();
+        System.out.println(a);
+
+
+    }
+}
