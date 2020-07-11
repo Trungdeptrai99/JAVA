f604ff06cf2115b309619dc76b15d71dbab511ea
Trungdeptrai99 <ktdarkness99@gmail.com>
2020-07-11 15:34:44 +0700

    update

--- ngày 6/Point2d3d/src/baitap/Point3D.java
+++ ngày 6/Point2d3d/src/baitap/Point3D.java
@@ -3,6 +3,27 @@ package baitap;
 public class Point3D extends Point2D {
     private float z = 0.0f;
 
-    public Point3D(float x, float y) {
+    public Point3D(float x, float y,float z) {
+        this.z= z;
+        super.setX(x);
+        super.setY(y);
+    }
+    public Point3D(){}
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+    public void setXYZ(float x,float y, float z){
+        this.z= z;
+        super.setX(x);
+        super.setY(y);
+    }
+    public float[] getXYZ(){
+        float arr[]={this.getX(),this.getY(),this.getZ()};
+        return arr;
     }
 }
