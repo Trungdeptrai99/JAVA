f56f20396c3b80112274acc4eff673d9cdd1fc8f
Trungdeptrai99 <ktdarkness99@gmail.com>
2020-07-21 15:30:00 +0700

    Trung dep trai

--- Week3/Day2/ListStudents/src/TestMap.java
+++ Week3/Day2/ListStudents/src/TestMap.java
@@ -1,23 +1,24 @@
 import java.util.*;
 public class TestMap {
     public static void main(String[] args) {
-      Map<String, Float> hashMap = new HashMap<>();
-      hashMap.put("Smith", 30f);
-      hashMap.put("Anderson", 31f);
-      hashMap.put("Lewis", 29f);
-      hashMap.put("Cock", 29f);
+      Map<String,Float> hashMap = new HashMap<>();
+      hashMap.put("Smith", 30.5f);
+      hashMap.put("Anderson", 31.6f);
+      hashMap.put("Lewis", 29.7f);
+      hashMap.put("Cock", 29.8f);
       System.out.println("Display entries in HashMap");
       System.out.println(hashMap + "\n");
 
-        Map<String, Integer> treeMap = new TreeMap(hashMap);
+        Map<String,Float> treeMap = new TreeMap(hashMap);
         System.out.println("Display entries in ascending order of key Trung Đẹp Trai");
         System.out.println(treeMap);
 
         Map<String, Integer> linkedHashMap = new LinkedHashMap<>(16, 0.75f, true);
         linkedHashMap.put("Smith", 30);
-        linkedHashMap.put("Anderson", 31);
+        linkedHashMap.put("Anderson", 33);
         linkedHashMap.put("Lewis", 32);
-        linkedHashMap.put("Cook", 33);
+        linkedHashMap.put("Cook", 31);
+        System.out.println(linkedHashMap);
         System.out.println("\nThe age for " + "Lewis is " + linkedHashMap.get("Lewis"));
     }
 }
